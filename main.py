4b776975622829be45ac54738399c4eb398952a9
SamuelCBird <samuel@samuelcbird.co.uk>
2022-04-03 17:52:47 +0100

    added html page, started writing sudoku solving

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
+from solver_v1.functions.functions import horizontal_check
+
+
 app = FastAPI();
 
 
 @app.get('/')
 async def root():
   return FileResponse("public/index.html")
+
+@app.get('/test')
+async def horiz_test(input: int):
+  result: int = await horizontal_check(input)
+  return result
