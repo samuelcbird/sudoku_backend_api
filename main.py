357c2f3701200414065a35ba1c4be18deb3518ee
SBird <samuel@samuelcbird.co.uk>
2022-04-10 22:36:01 +0100

    changed landing page, added info, trying stylings | also updated endpoints

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from solver_v1.main import backtracker
+from solver_v2.main import solver_v2
 
 # Schema for posting a puzzle to be solved
 class SolveRequest(BaseModel):
@@ -20,10 +21,14 @@ app = FastAPI()
 async def root():
   return FileResponse("public/index.html")
 
-@app.post('/v1/', response_model=SolveResponse)
-async def solver_v1(request: SolveRequest):
-  solution: list[int] = backtracker(request.puzzle)
-  return { 'input': request.puzzle, 'solution': solution }
+@app.post('/solve/', response_model=SolveResponse)
+async def solve(request: SolveRequest, v1: bool = False):
+  if not v1:
+    solution: list[int] = solver_v2(request.puzzle)
+    return { 'input': request.puzzle, 'solution': solution }
+  else:
+    solution: list[int] = backtracker(request.puzzle)
+    return { 'input': request.puzzle, 'solution': solution}
   
 
 if __name__ == "__main__":
