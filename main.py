455bfee65710f4da835bddfc02dff10220eaac80
SBird <samuel@samuelcbird.co.uk>
2022-07-09 12:01:18 +0100

    add CORS headers to response

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ async def solve(request: SolveRequest):
   try:
     solver = Solution(request.puzzle)
     solution: list[int] = solver.full_solution()
+    headers = { "Access-Control-Allow-Origin": "*" }
     response = { 'input': request.puzzle, 'solution': solution }
-    return JSONResponse(content=response)
+    return JSONResponse(content=response, headers=headers)
   except Exception as E:
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(E))
 
@@ -37,8 +38,9 @@ async def help(request: HelperRequest):
   try:
     solver = Solution(request.unsolved_puzzle, request.attempted_puzzle)
     incorrect_indexes: list[int] = solver.show_incorrect_answers()
+    headers = { "Access-Control-Allow-Origin": "*" }
     response = { 'attempted_puzzle': request.attempted_puzzle, 'incorrect_indexes': incorrect_indexes }
-    return JSONResponse(content=response)
+    return JSONResponse(content=response, headers=headers)
   except Exception as E:
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(E))
 
